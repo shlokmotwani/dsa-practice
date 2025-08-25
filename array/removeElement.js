edb930258c1d5c23169cb1cce90fd9851b2091a1
Shlok Motwani <shlok.codes@gmail.com>
2025-08-25 15:34:15 +0530

    fix: optimised two-pointer solution for remove element problem

--- array/removeElement.js
+++ array/removeElement.js
@@ -4,31 +4,15 @@
  * @return {number}
  */
 var removeElement = function (nums, val) {
-  // sort the array
-  nums.sort((a, b) => a - b);
-
-  let start = -1;
-  let found = false;
-  let count = 0;
+  let k = 0; // index of non-val element
   for (let i = 0; i < nums.length; i++) {
-    if (!found && nums[i] === val) {
-      start = i;
-      found = true;
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
     }
-    if (found && nums[i] !== val) {
-      count = i - start;
-      nums.splice(start, count);
-      start = -1;
-      break;
-    }
-  }
-
-  //check if the value to be removed exists in the end of the array
-  if (start !== -1) {
-    nums.splice(start);
   }
   console.log(nums);
-  return nums.length;
+  return k;
 };
 
 console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)); // 5, [ 0, 0, 1, 3, 4 ]
