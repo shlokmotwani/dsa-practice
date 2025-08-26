4f57054312d7bda4a4d9af27f422c794329bc904
Shlok Motwani <shlok.codes@gmail.com>
2025-08-26 14:32:18 +0530

    optimised solution for contains duplicate

--- array/containsDuplicate.js
+++ array/containsDuplicate.js
@@ -1,12 +1,8 @@
 var containsDuplicate = function (nums) {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums.indexOf(nums[i], i + 1) !== -1) {
-      return true;
-    }
-  }
-  return false;
+  let found = new Set(nums);
+  return found.size !== nums.length;
 };
 
-console.log(containsDuplicate([1,2,3,1]))
-console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
-console.log(containsDuplicate([1,2,3]))
+console.log(containsDuplicate([1, 2, 3, 1]));
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
+console.log(containsDuplicate([1, 2, 3]));
