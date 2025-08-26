b767fd96654bcd7822926d79af8b5a56591b2f57
Shlok Motwani <shlok.codes@gmail.com>
2025-08-26 13:14:49 +0530

    optimised solution for single number

--- array/singleNumber.js
+++ array/singleNumber.js
@@ -2,12 +2,11 @@
 // url: https://leetcode.com/problems/single-number
 
 var singleNumber = function (nums) {
-  nums.sort((a, b) => b - a);
-  for (let i = 0; i < nums.length; i += 2) {
-    if (i === nums.length - 1 || nums[i] !== nums[i + 1]) {
-      return nums[i];
-    }
+  let start = 0;
+  for (let i = 0; i < nums.length; i++) {
+    start ^= nums[i];
   }
+  return start;
 };
 
 console.log(singleNumber([2, 2, 1]));
