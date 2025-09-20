d9c01c5afba74679c6e8f4f9852afb631daf979d
Shlok Motwani <shlok.codes@gmail.com>
2025-09-20 12:13:58 +0530

    optimised: closest divisor

--- array/closestDivisor.js
+++ array/closestDivisor.js
@@ -1,18 +1,17 @@
 function closestDivisor(n, m){
-    for(let i=1; i<=Math.abs(n)**(1/2); i++){
-        let rightDivisor = (n+i)%m === 0 ? (n+i) : 0;
-        let leftDivisor = (n-i)%m === 0 ? (n-i) : 0;
-        if(leftDivisor && rightDivisor){
-            return Math.abs(leftDivisor) > Math.abs(rightDivisor) ? leftDivisor : rightDivisor;
-        }
-        else if(leftDivisor){
-            return leftDivisor;
-        }
-        else if(rightDivisor){
-            return rightDivisor;
-        }
-
-    }
+   let q = Math.floor(n/m);
+   let n1 = m*q;
+   let n2;
+   if(n*m < 0){
+    //signs of n and m are opposite
+    n2 = m * (q-1);
+   }
+   else{
+    n2 = m * (q+1);
+   }
+   let diff1 = Math.abs(n - n1);
+   let diff2 = Math.abs(n - n2);
+   return diff1<diff2? n1 : n2;
 }
 
 console.log(closestDivisor(13, 4));
