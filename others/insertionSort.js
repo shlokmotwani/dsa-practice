101f4c5eb28f087bc1b22e5aeb416143fd502243
Shlok Motwani <shlok.codes@gmail.com>
2025-11-02 18:31:55 +0530

    insertion sort assuming the right array to be sorted

--- others/insertionSort.js
+++ others/insertionSort.js
@@ -13,7 +13,29 @@ function insertionSort(arr) {
   }
 }
 
+function insertionSort2(arr) {
+  let N = arr.length;
+  for (let i = N - 2; i >= 0; i--) {
+    // console.log(`i => ${i}`);
+    for (j = N - 1; j > i; j--) {
+      // console.log(`j => ${j}`);
+      if (arr[i] > arr[j]) {
+        // console.log("Array before swap : " + arr);
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        // console.log("Array after swap : " + arr);
+      }
+    }
+  }
+}
+
 let arr = [10, 3, 6, 8, 2, 5];
-console.log(arr);
+console.log("Original Array : " + arr);
 insertionSort(arr);
-console.log(arr);
+console.log("Array using 1st sorting method : " + arr);
+
+arr = [10, 3, 6, 8, 2, 5];
+console.log("Original Array : " + arr);
+insertionSort2(arr);
+console.log("Array using 2nd sorting method : " + arr);
