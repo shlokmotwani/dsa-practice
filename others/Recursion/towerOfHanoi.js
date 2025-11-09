d9889a56fc69b45e3b29465a049cab5640044985
Shlok Motwani <shlok.codes@gmail.com>
2025-11-10 01:19:24 +0530

    tower of hanoi

---
+++ others/Recursion/towerOfHanoi.js
@@ -0,0 +1,11 @@
+function towerOfHanoi(n, A, B, C) {
+  if (n == 1) {
+    console.log(`Move disk ${n} from ${A} to ${C}`);
+    return;
+  }
+  towerOfHanoi(n - 1, A, C, B);
+  console.log(`Move disk ${n} from ${A} to ${C}`);
+  towerOfHanoi(n - 1, B, A, C);
+}
+
+towerOfHanoi(4, "A", "B", "C");
