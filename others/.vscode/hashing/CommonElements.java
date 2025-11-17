ec9976e8cc84248814be8ab555de5ebf1d633e92
Shlok Motwani <shlok.codes@gmail.com>
2025-11-17 15:59:42 +0530

    common elements

---
+++ others/.vscode/hashing/CommonElements.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommonElements {
+    public int[] solve(int[] A, int[] B) {
+        int N = A.length;
+        int M = B.length;
+
+        Map<Integer, Integer> map1 = new HashMap<>();
+        Map<Integer, Integer> map2 = new HashMap<>();
+
+        // initialize frequency of elements of A
+        for (int i = 0; i < N; i++) {
+            if (map1.get(A[i]) != null) {
+                map1.put(A[i], map1.get(A[i]) + 1);
+            } else {
+                map1.put(A[i], 1);
+            }
+        }
+
+        // initialize frequency of elements of B
+        for (int i = 0; i < M; i++) {
+            if (map2.get(B[i]) != null) {
+                map2.put(B[i], map2.get(B[i]) + 1);
+            } else {
+                map2.put(B[i], 1);
+            }
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int key : map1.keySet()) {
+            if (map2.containsKey(key)) {
+                int keyCount = Math.min(map1.get(key), map2.get(key));
+                for (int i = 0; i < keyCount; i++) {
+                    list.add(key);
+                }
+            }
+        }
+
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
